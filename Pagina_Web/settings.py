cee3ad32162c5b95cd6eb6a5551db8869f2b03de
bnja288 <benjamin.gomez.cavour@alumnos.uta.cl>
2025-08-08 02:47:05 -0400

    cambio DB en settings.py
    se realizo la migracion de la base de datos anterior "djangosqlite" que era la predeterminada a "MySQL" para poder ayudarnos a gestionar asi la informacion con mas detalle

--- Pagina_Web/settings.py
+++ Pagina_Web/settings.py
@@ -51,10 +51,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Pagina_Web.wsgi.application'
 
+#Base de datos en MySQl 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'restaurante_db',
+        'USER': 'djangouser',
+        'PASSWORD': 'django123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
