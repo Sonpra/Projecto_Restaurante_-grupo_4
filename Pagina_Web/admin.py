6c263e5dfca34b030a130d02c571be10513577b1
bnja288 <benjamin.gomez.cavour@alumnos.uta.cl>
2025-08-08 02:49:12 -0400

    Update admin.py

--- Pagina_Web/admin.py
+++ Pagina_Web/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Mesa, Plato, Pedido, DetallePedido
 
-# Este código le dice a Django: "Quiero ver y poder editar estos modelos
-# en el panel de administrador".
 admin.site.register(Mesa)
 admin.site.register(Plato)
 admin.site.register(Pedido)
